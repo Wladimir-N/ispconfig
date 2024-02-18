8c01455c0ea1eeac9907d12d937ebf475a1f38d7
Wladimir-N <46901390+Wladimir-N@users.noreply.github.com>
2024-02-18 18:57:46 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -16,6 +16,7 @@ if [ $exitstatus = 0 ]; then
 	if [ $OPTION = 1 ]; then
 		PHP=$(whiptail --title "Меню программы установки" --menu \
      "Выберите версию php" 23 77 16 \
+     "8.3" " " \
      "8.2" " " \
      "8.1" " " \
      "8.0" " " \
@@ -55,6 +56,7 @@ if [ $exitstatus = 0 ]; then
 	elif [ $OPTION = 2 ]; then
 		PHP=$(whiptail --title "Меню программы установки" --menu \
      "Выберите версию php" 23 77 16 \
+     "8.3" " " \
      "8.2" " " \
      "8.1" " " \
      "8.0" " " \
